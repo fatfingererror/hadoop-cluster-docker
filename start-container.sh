fb77a0823101bf9d4b76042da575c9f7b157d7a7
Theodoros Oikonomou <theodoros.oikonomou.ltd@gmail.com>
2016-07-29 21:01:49 +0100

    working mode with a single master node

--- start-container.sh
+++ start-container.sh
@@ -5,10 +5,9 @@ N=${1:-3}
 
 
 # start hadoop master container
-sudo docker rm -f hadoop-master &> /dev/null
+docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
-sudo docker run -itd \
-                --net=hadoop \
+docker run -itd \
                 -p 50070:50070 \
                 -p 8088:8088 \
                 --name hadoop-master \
@@ -16,19 +15,20 @@ sudo docker run -itd \
                 kiwenlau/hadoop:1.0 &> /dev/null
 
 
+                # --net=hadoop \
 # start hadoop slave container
-i=1
-while [ $i -lt $N ]
-do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
-	echo "start hadoop-slave$i container..."
-	sudo docker run -itd \
-	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
-	i=$(( $i + 1 ))
-done 
+# i=1
+# while [ $i -lt $N ]
+# do
+# 	 docker rm -f hadoop-slave$i &> /dev/null
+# 	echo "start hadoop-slave$i container..."
+# 	 docker run -itd \
+# 	                --net=hadoop \
+# 	                --name hadoop-slave$i \
+# 	                --hostname hadoop-slave$i \
+# 	                kiwenlau/hadoop:1.0 &> /dev/null
+# 	i=$(( $i + 1 ))
+# done 
 
 # get into hadoop master container
-sudo docker exec -it hadoop-master bash
+docker exec -it hadoop-master bash
